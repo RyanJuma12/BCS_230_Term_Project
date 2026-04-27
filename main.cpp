030c3af030a421f125ae4e24865a776e6675a1d5
Ryan Juma <ryanjjuma15@gmail.com>
2026-04-27 13:36:36 -0400

    Made first outline of main cpp file -Ryan

---
+++ main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include<ctime>
+#include<cmath>
+using namespace std;
+/*
+This is our term project for BCS_230
+It is a Fitness, Nutrition, Tracker designed to help people maintain shape
+Contributors: Ryan Juma, Tyler Busching, Taimur Qureshi, David Allendez, Daniel Barradas
+Date Created: 4/27/2026
+*/
+
+void displayMenu();
+
+int main() {
+
+}
+
+void displayMenu() {
+    cout << "====FitnessTracker Menu====\n";
+    cout << "1. Create Profile\n";
+    cout << "2. Update Profile\n";
+    cout << "3. Log Daily Caloric Intake\n";
+    cout << "4. Log Workouts\n";
+    cout << "5. Track Workouts over Time\n"; // File Graph Output
+    cout << "6. Track Body_Weight Over Time\n"; // File Graph Output
+    cout << "7. Maintenance Caloric Calculator\n";
+
+}
