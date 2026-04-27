195c6b755a090203be1f616dfda1f39736b32714
Ryan Juma <ryanjjuma15@gmail.com>
2026-04-27 16:11:58 -0400

    Modified Profile.h

--- Profile.h
+++ Profile.h
@@ -1,12 +1,23 @@
 //
-// Created by thoma on 4/27/2026.
+// Created on 4/27/2026.
 //
 
 #ifndef BCS_230_TERM_PROJECT_PROFILE_H
 #define BCS_230_TERM_PROJECT_PROFILE_H
+#include <iostream>
+#include <string>
+using namespace std;
 
 class Profile {
-public:
+    public:
+        void createProfile();
+        void updateProfile();
+        void displayProfile();
+    private:
+        string fname;
+        string lname;
+        string email;
+        string phone;
 };
 
 
